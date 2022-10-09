fb3cf5d7d1946c874416de8bcb86a8472d534cdc
Ahmed Sadman <ahmedsadman.211@gmail.com>
2022-10-09 22:22:55 +0600

    configure sqlalchemy

---
+++ server/app/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    CHECK_SAME_THREAD = False
---
+++ server/app/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from contextvars import ContextVar
+from .config import Config
+
+
+engine = create_engine(
+    Config.SQLALCHEMY_DATABASE_URI,
+    connect_args={'check_same_thread': Config.CHECK_SAME_THREAD}
+)
+
+session_factory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session_var: ContextVar = ContextVar('db_session')
+session = session_var.get
+
+Base = declarative_base()
