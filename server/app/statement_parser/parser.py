b0fe52f719629d95b34b6cb4a6ac1d70f112bda4
ahmedsadman <ahmedsadman.211@gmail.com>
2022-10-15 02:48:27 +0600

    use generated temp file for parser usage

--- server/app/statement_parser/parser.py
+++ server/app/statement_parser/parser.py
@@ -1,5 +1,7 @@
 import csv
 from datetime import datetime
+from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 from app import schemas
 from app.enums import TransactionType
@@ -11,10 +13,8 @@ class BaseParser:
     def __init__(self, file_path: str, config: ParserConfig):
         self.file_path = file_path
         self.config = config
-        self._read_statement()
     
-    def _read_statement(self):
-        temp_filename = 'tmp.csv'
+    def _read_statement(self, temp_filename):
         lines = None
         with open(self.file_path, 'r') as f:
             lines = f.readlines()
@@ -36,23 +36,31 @@ class BaseParser:
 
     def get_transactions(self) -> list[schemas.ParsedTransaction]:
         transactions = []
+
+        read_temp_file = Path(NamedTemporaryFile(suffix="csv").name)
+        str_path = read_temp_file.resolve()
+
+        with open(str_path, 'w') as f:
+            self._read_statement(str_path)
     
-        with open('tmp.csv', 'r') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                date = datetime.strptime(self.sanitize(row[self.config.date_colname]), self.config.date_parser)
-                type = TransactionType.WITHDRAW if row[self.config.withdraw_colname] not in ('-', '') else TransactionType.DEPOSIT
-                amount = row[self.config.withdraw_colname] if type == TransactionType.WITHDRAW else row[self.config.deposit_colname]
-                
-                transactions.append(
-                    schemas.ParsedTransaction(
-                        date=date, 
-                        description=row[self.config.desc_colname],
-                        type=type,
-                        amount=float(self.sanitize_amount(amount)),
-                        balance=float(self.sanitize_amount(row[self.config.balance_colname]))
+            with open(str_path, 'r') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    date = datetime.strptime(self.sanitize(row[self.config.date_colname]), self.config.date_parser)
+                    type = TransactionType.WITHDRAW if row[self.config.withdraw_colname] not in ('-', '') else TransactionType.DEPOSIT
+                    amount = row[self.config.withdraw_colname] if type == TransactionType.WITHDRAW else row[self.config.deposit_colname]
+                    
+                    transactions.append(
+                        schemas.ParsedTransaction(
+                            date=date, 
+                            description=row[self.config.desc_colname],
+                            type=type,
+                            amount=float(self.sanitize_amount(amount)),
+                            balance=float(self.sanitize_amount(row[self.config.balance_colname]))
+                        )
                     )
-                )
+
+            read_temp_file.unlink()
 
         return transactions
 
