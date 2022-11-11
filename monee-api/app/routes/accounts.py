5ceeb6dcfcc529f382e31b89fd650628b5ec35a4
ahmedsadman <ahmedsadman.211@gmail.com>
2022-11-12 00:15:02 +0600

    fix redirects on conventional routes

--- monee-api/app/routes/accounts.py
+++ monee-api/app/routes/accounts.py
@@ -13,13 +13,13 @@ router = APIRouter(
 )
 
 
-@router.post('/', status_code=201, response_model=schemas.Account)
+@router.post('', status_code=201, response_model=schemas.Account)
 async def create_account(account: schemas.AccountCreate):
     db_account = await AccountStorage.create_account(account)
     return db_account
 
 
-@router.get('/', response_model=list[schemas.Account])
+@router.get('', response_model=list[schemas.Account])
 async def list_all_accounts():
     return await AccountStorage.list_all_accounts()
 
