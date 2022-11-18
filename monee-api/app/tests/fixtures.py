e8ef149c9a067b42fdd543ebca07944d0385d924
ahmedsadman <ahmedsadman.211@gmail.com>
2022-11-18 19:29:30 +0600

    add tests for account routes

--- monee-api/app/tests/fixtures.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-from app.main import app
-
-
-@pytest.fixture
-def client():
-    with TestClient(app) as c:
-        yield c
---
+++ monee-api/app/tests/routes/test_accounts.py
@@ -0,0 +1,39 @@
+import pytest
+from app.tests.async_client import async_client
+from app.storage import AccountStorage
+from app import schemas
+
+
+mock_account_data = {
+    'title': 'Test',
+    'account_no': '123',
+    'bank_identifier': 'scb'
+}
+
+
+@pytest.fixture()
+async def account():
+    from app.db import session
+
+    acc = await AccountStorage.create_account(schemas.AccountCreate.parse_obj(mock_account_data))
+
+    await session().commit()
+    await session().refresh(acc)
+    yield acc
+
+
+async def test_create_account():
+    response = await async_client().post('/accounts', json=mock_account_data)
+    assert response.status_code == 201
+
+    res_json = response.json()
+    assert res_json['title'] == mock_account_data['title']
+
+
+async def test_list_account(account):
+    response = await async_client().get('/accounts')
+    assert response.status_code == 200
+
+    res_json = response.json()
+    assert len(res_json) == 1
+    assert res_json[0]['title'] == account.title
