7fdcea4412f598f5156de47ed68d36283e2a3b60
ahmedsadman <ahmedsadman.211@gmail.com>
2022-12-26 23:56:07 +0600

    add material ui

---
+++ monee-client/src/SideDrawer/index.tsx
@@ -0,0 +1,11 @@
+import { Drawer} from "@mui/material";
+
+
+function SideDrawer() {
+    return (<Drawer variant="permanent">
+        <div>Item 1</div>
+        <div>Item 2</div>
+    </Drawer>);
+}
+
+export default SideDrawer;
