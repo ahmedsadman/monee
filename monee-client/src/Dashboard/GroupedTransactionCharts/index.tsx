e71f4f27d048bf2090b9eacb306ed8d3625d9872
Ahmed Sadman <ahmedsadman.211@gmail.com>
2023-01-16 03:18:41 +0600

    add date option in grouped by month transaction chart

--- monee-client/src/Dashboard/GroupedTransactionCharts/index.tsx
+++ monee-client/src/Dashboard/GroupedTransactionCharts/index.tsx
@@ -1,11 +1,21 @@
-import { Card, CardContent } from "@mui/material";
+import { useCallback, useState } from "react";
+import { Moment } from "moment";
+import { Card, CardContent, Box, Typography } from "@mui/material";
 import useTransactionGroupByMonth from "../../data-hooks/useTransactionGroupByMonth";
 import { GroupedTransaction } from "../../common/types";
 import BarSeriesPlot from "../../common/components/BarSeriesPlot";
+import DateRangePicker, {
+  defaultPresetOptions,
+} from "../../common/components/DateRangePicker";
 
 function GroupedTransactionCharts() {
+  const [startDate, setStartDate] = useState<Moment | null>(null);
+  const [endDate, setEndDate] = useState<Moment | null>(null);
   const { groupedByMonthDeposits, groupedByMonthWithdrawls } =
-    useTransactionGroupByMonth();
+    useTransactionGroupByMonth(startDate, endDate);
+
+  const presetOptions = defaultPresetOptions.slice(1);
+  presetOptions[presetOptions.length - 1].default = true;
 
   const getXYCoord = (data: GroupedTransaction[]) =>
     data.map((item) => ({
@@ -13,9 +23,31 @@ function GroupedTransactionCharts() {
       y: item.sum,
     }));
 
+  const handleDateRangeChange = useCallback(
+    (startDate: Moment | null, endDate: Moment | null) => {
+      setStartDate(startDate);
+      setEndDate(endDate);
+    },
+    []
+  );
+
   return (
     <Card sx={{ mt: 3 }}>
       <CardContent>
+        <Typography
+          sx={{ fontSize: 14, pb: 3 }}
+          color="text.secondary"
+          gutterBottom
+        >
+          Withdraw/Deposit by month
+        </Typography>
+        <Box sx={{ mb: 3, ml: 3 }}>
+          <DateRangePicker
+            onChange={handleDateRangeChange}
+            presetOptions={presetOptions}
+          />
+        </Box>
+
         <BarSeriesPlot
           series={[
             {
--- monee-client/src/data-hooks/useTransactionGroupByMonth.tsx
+++ monee-client/src/data-hooks/useTransactionGroupByMonth.tsx
@@ -1,8 +1,13 @@
 import axios from "axios";
+import { useCallback } from "react";
+import { Moment } from "moment";
 import { useEffect, useState } from "react";
 import { GroupedTransaction } from "../common/types";
 
-function useTransactionGroupByMonth() {
+function useTransactionGroupByMonth(
+  startDate: Moment | null,
+  endDate: Moment | null
+) {
   const [groupedByMonth, setGroupedByMonth] = useState<
     GroupedTransaction[] | undefined
   >();
@@ -13,11 +18,16 @@ function useTransactionGroupByMonth() {
     GroupedTransaction[] | undefined
   >();
 
-  const fetchGroupedByMonth = () => {
+  const fetchGroupedByMonth = useCallback(() => {
+    if (!startDate || !endDate) {
+      return;
+    }
+
     const params = {
-      start_date: "2022-1-1",
-      end_date: "2022-12-30",
+      start_date: startDate.format("YYYY-MM-DD"),
+      end_date: endDate.format("YYYY-MM-DD"),
     };
+
     axios.get("/api/transactions/grouped/month", { params }).then((resp) => {
       const { data }: { data: GroupedTransaction[] } = resp;
       data.sort((a, b) => {
@@ -32,20 +42,20 @@ function useTransactionGroupByMonth() {
         return 0;
       });
       const withdrawls = resp.data.filter(
-        (item: GroupedTransaction) => item.type == "withdraw"
+        (item: GroupedTransaction) => item.type === "withdraw"
       );
       const deposits = resp.data.filter(
-        (item: GroupedTransaction) => item.type == "deposit"
+        (item: GroupedTransaction) => item.type === "deposit"
       );
       setGroupedByMonth(resp.data);
       setGroupedByMonthDeposits(deposits);
       setGroupedByMonthWithdrawls(withdrawls);
     });
-  };
+  }, [startDate, endDate]);
 
   useEffect(() => {
     fetchGroupedByMonth();
-  }, []);
+  }, [fetchGroupedByMonth]);
 
   return { groupedByMonth, groupedByMonthWithdrawls, groupedByMonthDeposits };
 }
