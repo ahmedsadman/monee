91bc7665161b78a3d029039fab059165215d29c8
Ahmed Sadman <ahmedsadman.211@gmail.com>
2023-01-27 12:25:27 +0600

    add top level date skeleton

--- monee-client/src/pages/Dashboard/index.tsx
+++ monee-client/src/pages/Dashboard/index.tsx
@@ -1,10 +1,21 @@
+import { AppBar, Paper } from "@mui/material";
 import TransactionSummary from "./TransactionSummary";
 import GroupedTransactionCharts from "./GroupedTransactionCharts";
 import DescriptionGroup from "./DescriptionGroup";
+import DateRangePicker from "../../common/components/DateRangePicker";
 
 function Dashboard() {
   return (
     <>
+      <AppBar
+        position="sticky"
+        elevation={0}
+        sx={{ mb: 3, backgroundColor: "white", color: "black" }}
+      >
+        <Paper variant="outlined" sx={{ padding: 3 }}>
+          <DateRangePicker onChange={() => {}} />
+        </Paper>
+      </AppBar>
       <TransactionSummary />
       <GroupedTransactionCharts />
       <DescriptionGroup />
